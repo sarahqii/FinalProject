7e4a0d8bf0cf0ed608b07c1ad207fc9a6b81b655
sarahqii <152104311+sarahqii@users.noreply.github.com>
2025-04-27 22:02:33 -0500

    Add files via upload

---
+++ Agent.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.finalprojectparti;
+import java.util.*;
+/**
+ *
+ * @author chenhenr
+ */
+public class Agent {
+    private int index;
+    private int pairNum;
+    private ArrayList<Integer> pairAgents;
+    private int agentNum;
+    private double p;
+
+    public Agent(int index, int pairNum, int agentNum, double p){
+        this.index = index;
+        this.pairNum = pairNum;
+        this.pairAgents = new ArrayList<>();
+        this.agentNum = agentNum;
+        this.p = p;
+    }
+    
+    public void ranPair(int i, Agent agenti){
+        Random ran = new Random();
+        double ranN = ran.nextDouble(11)/10;
+        if(index == i){
+            return;
+        }
+        if(agenti.checkPair(index)){
+            pairNum++;
+            pairAgents.add(i);
+        }
+        else{
+            if(ranN <= p){
+                pairNum++;
+                pairAgents.add(i);
+            }
+            else{ 
+                return;
+            }
+        }
+    }
+    public boolean checkPair (int i){
+        return pairAgents.contains(i);
+    }
+    public Agent getSelf(){
+        return this;
+    }
+    
+    public ArrayList<Integer> getPairs(){
+        return pairAgents;
+    }
+    
+    public int getIndex(){
+        return index;
+    }
+}
