62d1100443f53c011806c2f7c8c8c1d8b9825dd9
sarahqii <152104311+sarahqii@users.noreply.github.com>
2025-04-30 16:38:28 -0500

    Update Network.java

--- Network.java
+++ Network.java
@@ -15,6 +15,7 @@ public class Network {
     private int agentNum;
     private double p;
     private Random ran;
+    private int gridRow;
     
     public Network(int agentNum, double p){
         this.agents = new Agent[agentNum + 1];
@@ -24,6 +25,7 @@ public class Network {
         this.agentNum = agentNum;
         this.p = p;
         this.ran = new Random();
+        gridRow = 0;
     }
     
     public void ranPair(){
@@ -79,5 +81,109 @@ public class Network {
         }
     }
     
+    public void makeGrid(){
+        int remainder = agentNum % 3;
+        if(remainder == 0){
+            gridRow = agentNum / 3;
+        }
+        else if(remainder == 1){
+            gridRow = agentNum / 3;
+        }
+        else{
+            gridRow = (agentNum / 3) + 1;
+        }
+    }
+    
+    public int coorToInt(int[] coor){
+        return (coor[0] - 1)*3 + coor[1];
+    }
+    public int[] intToCoor(int i){
+        int[] coor = new int[2];
+        coor[0] = ((i - 1) / 3) + 1; //Row
+        coor[1] = (i - 1) % 3 + 1; //Col
+        return coor;
+    }
+    
+    public int rowIncrement(int row){
+        int newRow = row + 1;
+        if(newRow > gridRow){
+            newRow = newRow % gridRow;
+            return newRow;
+        }
+        else{
+            return newRow;
+        }
+    }
+    public int rowDecrement(int row){
+        int newRow = row - 1;
+        if(newRow < 1){
+            newRow = newRow + gridRow;
+            return newRow;
+        }
+        else{
+            return newRow;
+        }
+    }
+    public int colIncrement(int col){
+        int newCol = col + 1;
+        if(newCol > 3){
+            newCol = newCol % 3;
+            return newCol;
+        }
+        else{
+            return newCol;
+        }
+    }
+    public int colDecrement(int col){
+        int newCol = col - 1;
+        if(newCol < 1){
+            newCol = newCol + 3;
+            return newCol;
+        }
+        else{
+            return newCol;
+        }
+    }
+    
+    
+    
+    public void gridPair(){
+        makeGrid();
+      //  System.out.println("Row is " + gridRow);
+        int[] coor0 = new int[2];
+        int[] coor1 = new int[2]; //Upgrid
+        int[] coor2 = new int[2]; //Rightgrid
+        int[] coor3 = new int[2]; //Downgrid
+        int[] coor4 = new int[2]; //Leftgrid
+        int iUp, iRight, iDown, iLeft;
+        for(int i = 1; i <= 3*gridRow; i++){
+            coor0 = intToCoor(i);
+            
+            coor1[0] = coor0[0];
+            coor1[1] = colDecrement(coor0[1]);
+            iLeft = coorToInt(coor1);
+            
+            coor2[0] = rowIncrement(coor0[0]);
+            coor2[1] = coor0[1];
+            iDown = coorToInt(coor2);
+            
+            coor3[0] = coor0[0];
+            coor3[1] = colIncrement(coor0[1]);
+            iRight = coorToInt(coor3);
+            
+            coor4[0] = rowDecrement(coor0[0]);
+            coor4[1] = coor0[1];
+            iUp = coorToInt(coor4);
+            
+            agents[i].unilateralPair(iUp);
+            agents[i].unilateralPair(iRight);
+            agents[i].unilateralPair(iDown);
+            agents[i].unilateralPair(iLeft);
+        }
+    }
+    
+    public Agent getAgent(int i) {
+        return agents[i];
+    }    
     
 }
