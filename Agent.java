fff487325a643fff99f6544dc511b1ffc145f597
sarahqii <152104311+sarahqii@users.noreply.github.com>
2025-05-03 22:34:19 -0500

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -1,126 +1,94 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.finalprojectparti;
 import java.util.*;
 /**
  *
  * @author chenhenr
  */
-// Main method to run the program and prompt user interaction.
-public class FinalProjectPartI {
+public class Agent {
+    private int index;
+    private int pairNum;
+    private PriorityQueue<Integer> pairAgents;
+    private int agentNum;
+    private double p;
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Welcome to the Network Generator");
-        System.out.println("Please select a mode");
-        System.out.println("1. Network 1(Example)");
-        System.out.println("2. Random Network");
-        System.out.println("3. 2D4N Regular Lattice");
-        
-        int mode = in.nextInt();
-        
-        while(mode != 1&&mode != 2&&mode != 3) {
-            System.out.println("Invalid number! Please enter another number: ");
-            mode = in.nextInt();
-        }
-        // The user will choose between the three modes.
-        if(mode == 1){
-            System.out.println("Generating Network 1...");
-            Network network = new Network(12, 1);
-            Agent agent;
-            agent = network.getAgent(1);
-            agent.unilateralPair(4);
-            agent.unilateralPair(7);
-            agent.unilateralPair(10);
-            
-            agent = network.getAgent(2);
-            agent.unilateralPair(7);
-            agent.unilateralPair(9);
-            agent.unilateralPair(11);       
-            
-            agent = network.getAgent(4);
-            agent.unilateralPair(1);
-            agent.unilateralPair(5);
-
-            agent = network.getAgent(5);
-            agent.unilateralPair(4);
-            
-            agent = network.getAgent(7);
-            agent.unilateralPair(1);
-            agent.unilateralPair(2);
-            
-            agent = network.getAgent(8);
-            agent.unilateralPair(10);
-            
-            agent = network.getAgent(9);
-            agent.unilateralPair(10);
-            agent.unilateralPair(12);
-            
-            agent = network.getAgent(10);
-            agent.unilateralPair(1);
-            agent.unilateralPair(8);
-            agent.unilateralPair(9);
-            
-            agent = network.getAgent(11);
-            agent.unilateralPair(2);
-            
-            agent = network.getAgent(12);
-            agent.unilateralPair(9);
-            
-            network.printAllPairs();
-            network.writeToFile("output.txt");
-
-        }
-        else if(mode == 2){
-            System.out.println("Enter number of agents in the range of [10, 1000]");
-            int N = in.nextInt();
-            while (N < 10||N > 1000) {
-                System.out.println("Invalid number! Please enter another number!");
-                N = in.nextInt();
-            }
-            System.out.println("Enter the probability in the range of [1/N, 1]");
-            double p = in.nextDouble();
-            while (p < 1.0/N || p > 1.0) {          
-                System.out.println("Invalid number! Please enter another number!");
-                p = in.nextDouble();
-            }
-            Network network = new Network(N, p);
-            network.ranPair();
-            network.printAllPairs();
-            network.writeToFile("output.txt");
-        }
-        else if(mode == 3){
-            System.out.println("Enter number of agents in the range of [9, 1000]");
-            int N = in.nextInt();
-            while (N < 9 || N > 1000) {
-                System.out.println("Invalid number! Please enter another number!");
-                N = in.nextInt();
-            }
-            N = adjustAgentNum(N);
-            Network network = new Network(N, 1.0);
-            network.gridPair();
-            network.printAllPairs();
-            network.writeToFile("output.txt");
-        
-        }
-        
-        
+    public Agent(int index, int pairNum, int agentNum, double p){
+        this.index = index;
+        this.pairNum = pairNum;
+        this.pairAgents = new PriorityQueue<>();
+        this.agentNum = agentNum;
+        this.p = p;
     }
-    // Adjusts the number of agents to be a multiple of 3 for lattice construction.
-    public static int adjustAgentNum(int agentNum){
-        int row = 0;
-        int remainder = agentNum % 3;
-        if(remainder == 0){
-            row = agentNum / 3;
+    /*
+    public void ranPair(int i, Agent agenti){
+        Random ran = new Random();
+        double ranN = ran.nextInt(11)/10.0;
+        //ranN = 1;
+        if(index == i){
+            return;
         }
-        else if(remainder == 1){
-            row = agentNum / 3;
+        if(agenti.checkPair(index)){
+            pairNum++;
+            pairAgents.add(i);
         }
         else{
-            row = (agentNum / 3) + 1;
+            if(ranN > p){
+                pairNum++;
+                pairAgents.add(i);
+            }
+            else{ 
+                return;
+            }
         }
-        return row * 3;
+    }
+    */
+    public void ranPair(int i, Agent agenti){
+        Random ran = new Random();
+        double ranN = ran.nextInt(11)/10.0;
+        if(ranN > p){
+            pair(i, agenti);
+        }
+        else{ 
+            return;
+        }
+    }
+    
+    
+    public void pair(int i, Agent agenti){
+        if(index == i){
+            return;
+        }
+        if(agenti.checkPair(index)){
+            pairNum++;
+            pairAgents.add(i);
+        }
+        else{
+            pairNum++;
+            pairAgents.add(i);
+            agenti.getPairs().add(index);
+        }
+    }
+    
+    
+    public boolean checkPair (int i){
+        return pairAgents.contains(i);
+    }
+    public Agent getSelf(){
+        return this;
+    }
+    
+    public PriorityQueue<Integer> getPairs(){
+        return pairAgents;
+    }
+    
+    public void unilateralPair(int i){
+        pairAgents.add(i);
+    }
+    
+    public int getIndex(){
+        return index;
     }
 }
